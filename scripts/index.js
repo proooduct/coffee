d6241280bf5911688a8fdd248742472d28b0ad6c
proooduct <146192277+proooduct@users.noreply.github.com>
2023-11-01 12:30:29 +0000

    Final Code

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+// CDE: GitHub Codespaces Intro
+// Data Types
+// Primitive Data Types: String, Number, Boolean, undefined, null
+// (specific usecase) BigInt and Symbol
+// Strings: "", '', ``
+// Template Literal: `${value}` (This is called interpolation = insert values)
+// Numbers: Operators: +, -, *, /, % (Remainder)
+// Type Coercion: Automatic type changing
+
+// implicit: automatic type change: type coercion
+// example: 2 + '2' => 22
+// explicit: type casting (from one type to another intentionally)
+// type cast: Number(), String(), Boolean()
+
